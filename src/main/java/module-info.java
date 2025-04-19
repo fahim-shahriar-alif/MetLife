e21ac1629b8cf6d533aa3b296a17652f72bb476f
InzamamulHoque <ihrafi.99@gmail.com>
2025-04-20 01:04:36 +0600

    Implement HR Manager Dashboard and controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,4 +9,7 @@ module com.example.alif.metlife {
     exports com.example.alif.metlife;
     exports com.example.alif.metlife.Alif_2221079;
     opens com.example.alif.metlife.Alif_2221079 to javafx.fxml;
+
+    exports com.example.alif.metlife.InzamamulHoque_1910014;
+    opens com.example.alif.metlife.InzamamulHoque_1910014 to javafx.fxml;
 }
