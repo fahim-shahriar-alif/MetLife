4fe377ae02a064a6944822ccd5391eb42672589b
InzamamulHoque <ihrafi.99@gmail.com>
2025-04-22 01:33:07 +0600

    modified Emploee Information Controller and model class

--- src/main/java/com/example/alif/metlife/inzamamulhoque_1910014/JobRecruitmentViewController.java
+++ src/main/java/com/example/alif/metlife/inzamamulhoque_1910014/JobRecruitmentViewController.java
@@ -1,7 +1,41 @@
 package com.example.alif.metlife.inzamamulhoque_1910014;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+import javax.swing.text.TableView;
+
 public class JobRecruitmentViewController
 {
     @javafx.fxml.FXML
+    private TextField jobRequirementTextField;
+    @javafx.fxml.FXML
+    private TableColumn<EmployeeJob,String> jobDescriptionTableCloumn;
+    @javafx.fxml.FXML
+    private TextField jobPositionTextField;
+    @javafx.fxml.FXML
+    private TableView<EmployeeJob> jobRecruitmentTableView;
+    @javafx.fxml.FXML
+    private TextField jobDescriptionTextField;
+    @javafx.fxml.FXML
+    private TableColumn<EmployeeJob,String> jobPositionTableCloumn;
+    @javafx.fxml.FXML
+    private TableColumn<EmployeeJob,String> salaryRangeTableColumn;
+    @javafx.fxml.FXML
+    private TextField jobSalaryTextField;
+    @javafx.fxml.FXML
+    private TableColumn<EmployeeJob,String> jobRequirementTableCloumn;
+
+    @javafx.fxml.FXML
     public void initialize() {
-    }}
+    }
+
+    @javafx.fxml.FXML
+    public void backToDashboardButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void showInformationsButtonOnAction(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/example/alif/metlife/inzamamulhoque_1910014/TrainingWorkshopViewController.java
+++ src/main/java/com/example/alif/metlife/inzamamulhoque_1910014/TrainingWorkshopViewController.java
@@ -1,42 +1,39 @@
 package com.example.alif.metlife.inzamamulhoque_1910014;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 
 import javax.swing.text.TableView;
 
-public class SalaryCertificateViewController
+public class TrainingWorkshopViewController
 {
     @javafx.fxml.FXML
-    private TableColumn<SalaryCertificate, String> employeeSalaryTableColumn;
+    private DatePicker startDatePicker;
     @javafx.fxml.FXML
-    private TextField employeeIDInputTextField1;
+    private DatePicker endDatePicker;
     @javafx.fxml.FXML
-    private TableColumn<SalaryCertificate, String> employeeNameTableColumn;
+    private TableColumn<TrainingWorkshop,String> employeeNameTableColumn;
     @javafx.fxml.FXML
     private TextField employeeNameInputTextField;
     @javafx.fxml.FXML
-    private TableColumn<SalaryCertificate, String> employeeDesignationTableColumn;
+    private TableColumn<TrainingWorkshop,String> employeeIDTableColumn1;
     @javafx.fxml.FXML
-    private TextField employeeDesignationTextField;
+    private TableColumn<TrainingWorkshop,String> employeeIDTableColumn;
     @javafx.fxml.FXML
-    private TextField employeeSalaryTextField;
+    private TableColumn<TrainingWorkshop,String> salaryRangeTableColumn;
     @javafx.fxml.FXML
-    private TextField departmentNameTextField;
+    private TextField employeeIDInputTextField;
     @javafx.fxml.FXML
-    private TableColumn<SalaryCertificate, String> employeeIDTableColumn;
+    private TextField trainingOrWorkshopContentTextField;
     @javafx.fxml.FXML
     private TableView trainingWorkshopTableView;
     @javafx.fxml.FXML
-    private TableColumn<SalaryCertificate, String>departmentNameTableColumn;
+    private TableColumn<TrainingWorkshop,String> jobRequirementTableColumn;
 
     @javafx.fxml.FXML
     public void initialize() {
-    }
-
-    @javafx.fxml.FXML
-    public void downloadSalaryCertificateButtonOnAction(ActionEvent actionEvent) {
     }
 
     @javafx.fxml.FXML
